92f386ccb8aaefdb20a3104e33cadf8aeebef282
badunc <baduncan@yahoo.com>
2026-07-28 20:18:48 -0400

    Create constants for Sequent Multichemistry UPS
    Added constants for the Sequent Multichemistry UPS integration.

---
+++ custom_components/sequent_multichem_ups/const.py
@@ -0,0 +1,15 @@
+"""Constants for the Sequent Multichemistry UPS integration."""
+
+from __future__ import annotations
+
+DOMAIN = "sequent_multichem_ups"
+
+NAME = "Sequent Multichemistry UPS"
+
+MANUFACTURER = "Sequent Microsystems"
+
+DEFAULT_I2C_BUS = 1
+
+DEFAULT_I2C_ADDRESS = 0x30
+
+DEFAULT_SCAN_INTERVAL = 10
